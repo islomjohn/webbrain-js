d873da658d1b9d7b2ded02fe383d8b5dce66f6d6
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-09-15 18:55:36 +0300

    Add home task

---
+++ js/_hometask.js
@@ -0,0 +1,68 @@
+//! 1
+let a = 8;
+const P = 4 * a;
+console.log(`Perimetr ${P}`);
+
+//! 2 
+let squareA = 8;
+console.log(`Kvadrat yuzi ${squareA ** 2}`)
+
+//! 3 
+let shapeA = 8;
+let shapeB = 8;
+console.log(`S = a * b => ${shapeA * shapeB}`);
+console.log(`P = 2 * (a + b) => ${2 * (shapeA + shapeB)}`);
+
+//! 4 
+let d = 15;
+const PI = 3.14;
+console.log(`Doiraning yuzi: ${PI * d}`);
+
+//! 5 
+let cubeA = 3;
+const V = cubeA ** 3;
+const S = 6 * (cubeA ** 2)
+console.log(`V = a ** 3 => ${V}`);
+
+//! 6 
+let parA = 6,  
+    parB = 8, 
+    parC = 9;
+const ParV = parA * parB * parC;
+const ParS = 2 * (parA * parB + parB * parC + parA * parC);
+console.log(`V = a * b * c => ${ParV}`);
+console.log(`S = 2 * (a * b + b * c + a * c) => ${ParS}`);
+
+//! 7 
+let circleRadius = 15;
+const R = 2 * PI * circleRadius;
+const circleS = PI * (circleRadius ** 2)
+console.log(`R = 2 * PI * r => ${R}`)
+console.log(`R = PI * r ** 2 => ${circleS}`);
+
+//! 8 
+let num1 = 10,
+    num2 = 20;
+console.log((num1 + num2) / 2)
+
+//! 9
+let num3 = 10,
+    num4 = 20;
+console.log((num3 * num4) / (0.5));
+
+//! 10 
+let num5 = 8, 
+    num6 = 9, 
+    num7 = 10;
+console.log(num5 + num6 + num7)
+console.log(num5 * num6 * num7)
+console.log(num5 ** 2 )
+console.log(num6 ** 2 )
+console.log(num7 ** 2 )
+
+//! 11 
+
+
+
+
+

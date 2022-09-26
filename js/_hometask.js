f1513d7fc5df9e7b6b3651da1f08e6d92986a5eb
islomjohn <islomjohn.uz@gmail.com>
2022-09-26 22:34:09 +0300

    feat: Add home task

--- js/_hometask.js
+++ js/_hometask.js
@@ -2,11 +2,9 @@
 // A3B2C3A
 
 const makeEncode = (str) => {
-  for (let i = 0; i < str.length; i++) {
-    let count = 1;
-    
-  }
+  return str.split("").map((v,i) => `${+v + 1}` === "NaN" ? i !== 0 ? "-" +v : v: +v).join("").split("-").map(v => v[0].repeat(v[1] ? v[+1] : 1)).join("")
 }
+console.log(makeEncode("A5B3A2C"));
 
 //! 2
 var arr = [4, 10, 888, 99, 100]

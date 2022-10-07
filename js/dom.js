68cec92a766eb0b832d72ea558bddd88fc4555d8
islomjohn <islomjohn.uz@gmail.com>
2022-10-07 14:37:17 +0300

    feat: Add dom example

--- js/dom.js
+++ js/dom.js
@@ -1,21 +1,31 @@
 // console.log(document.getElementsByTagName("h1"));
 
 const title = document.querySelector("#title")
-let isActive = true;
-const onChange = () => {
-  isActive
-    ? (
-      title.style.cssText = `
-        color: red;
-        background-color: green;
-        transition: all 1s ease;
-      `
-    ) : (
-      title.style.cssText = `
-        color: black;
-        background-color: transparent;
-        transition: all 1s ease;
-      `
-    )
-    isActive = !isActive
-}
+// let isActive = true;
+// const onChange = () => {
+//   isActive
+//     ? (
+//       title.style.cssText = `
+//         color: red;
+//         background-color: green;
+//         transition: all 1s ease;
+//       `
+//     ) : (
+//       title.style.cssText = `
+//         color: black;
+//         background-color: transparent;
+//         transition: all 1s ease;
+//       `
+//     )
+//     isActive = !isActive
+// }
+
+const btn = document.querySelector("#btn")
+
+btn.addEventListener("click", () => {
+  title.style.cssText = `
+    color: red;
+    background-color: green;
+    transition: all 1s ease;
+  `
+})

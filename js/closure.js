d23a85172dc666cb476314272a3c071ef6d07f33
islomjohn <islomjohn.uz@gmail.com>
2022-10-07 13:01:58 +0300

    feat: Add url Generator example to closure

--- js/closure.js
+++ js/closure.js
@@ -22,3 +22,37 @@ function createCalcFunction(n) {
 const calc = createCalcFunction(42)
 // console.log(calc);
 console.log(calc());
+
+//==================================
+//==================================
+
+function incrementer(n) {
+  return function(num) {
+    return n + num
+  }
+}
+const addOne = incrementer(1)
+console.log(addOne(10)); // 11
+console.log(addOne(20)); // 21
+
+const addTen = incrementer(10)
+console.log(addTen(100)); // 110
+console.log(addTen(1000)); // 1010
+
+//==================================
+//==================================
+
+const urlGenerator = (domen) => {
+  return function(url) {
+    return `https://${url}.${domen}`
+  }
+}
+
+const comUrl = urlGenerator("com")
+const ruUrl = urlGenerator("com")
+
+console.log(comUrl("google"));
+console.log(comUrl("netflix"));
+
+console.log(comUrl("yandex"));
+console.log(comUrl("vk"));
